50b53cc302b019346f5a2eedc142842a1a36d9ed
Debuga Dev <debugadev@gmail.com>
2024-06-27 00:15:32 -0300

    First settings

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './complements/styles.css'
+
+import { RouterView } from 'vue-router'
+
+import NavBar from './components/navbar.vue'
+</script>
+
+<template>
+  <NavBar />
+
+  <RouterView />
+</template>
